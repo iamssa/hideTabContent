66ed9f1bac58cc38221db00c6ba6d07727be7997
Alexandr Simakov <alexandrsimakovv@mail.ru>
2018-12-07 01:08:02 +0500

    add script

---
+++ script.js
@@ -0,0 +1,37 @@
+window.addEventListener('DOMContentLoaded', function() {
+
+    'use strict';
+
+    let tab = document.querySelectorAll('.info-header-tab'),
+        info = document.querySelector('.info-header'),
+        tabContent = document.querySelectorAll('.info-tabcontent');
+
+    function hideTabСontent (a) { // функция скрывает таб
+        for (let i = a; i < tabContent.length; i++) {
+            tabContent[i].classList.remove('show');
+            tabContent[i].classList.add('hide');
+        }
+    }
+
+    hideTabСontent(1);
+
+    function showTabСontent (b) { // функция показывает таб
+        if (tabContent[b].classList.contains('hide')) {
+            tabContent[b].classList.remove('hide');
+            tabContent[b].classList.add('show');
+        }
+    }
+
+    info.addEventListener('click', function(event) {
+        let target = event.target;
+        if (target && target.classList.contains('info-header-tab')) {
+            for (let i = 0; i < tab.length; i++) {
+                if (target == tab[i]) {
+                    hideTabСontent(0);
+                    showTabСontent(i);
+                    break;
+                }
+            }
+        }
+    });
+});
